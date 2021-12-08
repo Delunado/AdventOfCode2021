b4447f6f7fb289e7c7f8cc6b9eaa5d42d08c020f
Javier-Delunado <fjaviercamacholopez@gmail.com>
2021-12-08 11:25:13 +0100

    Problem 4 Part 1 & 2 solved!

--- AdventOfCode21/Problem4.cpp
+++ AdventOfCode21/Problem4.cpp
@@ -13,13 +13,17 @@ void Problem4::LoadProblem(std::string filepath)
 
 	if (file) {
 		while (!file.eof()) {
-			std::string nextBingoBoardLine;
-			file >> nextBingoBoardLine;
 
-			//Crear sistema para cargar los boards con numeros y que se vayan rellenando automáticamente. Te avisa cuando ya está lleno del todo. Hay que rellenarlo con
-			//BoardsNumbers, no enteros normales!
+			BingoBoard bingoBoard(5, 5);
 
-			Utils::Log(nextBingoBoardLine);
+			while (!bingoBoard.IsComplete()) {
+				int nextBingoBoardNumber;
+				file >> nextBingoBoardNumber;
+
+				bingoBoard.AddNumber(nextBingoBoardNumber);
+			}
+
+			_bingoBoards.push_back(bingoBoard);
 		}
 
 		file.close();
@@ -52,6 +56,48 @@ void Problem4::LoadProblemRandomNumbers(std::string filepath)
 	}
 }
 
+int Problem4::FindFirstWinningBoard()
+{
+	for (int i = 0; i < _randomNumbers.size(); i++) {
+		int currentRandomNumber = _randomNumbers[i];
+
+		for (int j = 0; j < _bingoBoards.size(); j++) {
+			if (_bingoBoards[j].CheckNumber(currentRandomNumber)) {
+				return _bingoBoards[j].CalculateScore();
+			}
+		}
+	}
+
+	return 0;
+}
+
+/// <summary>
+
+/// </summary>
+/// <returns></returns>
+int Problem4::FindLastWinningBoard()
+{
+	/// We process all the boards and save the latest score, 
+	//so when all random numbers are drawn, the last score will be the winner one.
+	int lastScore = 0;
+
+	for (int i = 0; i < _randomNumbers.size(); i++) {
+		int currentRandomNumber = _randomNumbers[i];
+
+		for (int j = 0; j < _bingoBoards.size(); j++) {
+			if (!_bingoBoards[j].IsWinning()) {
+				if (_bingoBoards[j].CheckNumber(currentRandomNumber)) {
+					lastScore = _bingoBoards[j].CalculateScore();
+				}
+			}
+		}
+	}
+
+	return lastScore;
+}
+
 void Problem4::Resolve()
 {
+	std::cout << "The final score of the first winning board is " << FindFirstWinningBoard() << std::endl;
+	std::cout << "The final score of the last winning board is " << FindLastWinningBoard() << std::endl;
 }
--- AdventOfCode21/Problem4.h
+++ AdventOfCode21/Problem4.h
@@ -16,13 +16,129 @@ struct BingoNumber {
 class BingoBoard {
 private:
 	std::vector<std::vector<BingoNumber>> _bingoBoard;
+	int _rowSize;
+	int _columnSize;
+	int _currentRow;
+	int _currentColumn;
+	bool _isComplete;
+	bool _isWinning;
+	int _winNumber;
 
 public:
-	BingoBoard(std::vector<std::vector<BingoNumber>> bingoBoard) {
-		_bingoBoard = bingoBoard;
+	BingoBoard(int rowSize, int columSize) : _bingoBoard(std::vector<std::vector<BingoNumber>>()), _rowSize(rowSize), _columnSize(columSize),
+		_currentRow(0), _currentColumn(0), _isComplete(false), _isWinning(false), _winNumber(0) {
+
+		for (int i = 0; i < _rowSize; i++) {
+			_bingoBoard.push_back(std::vector<BingoNumber>());
+		}
 	}
 
+	void AddNumber(int number) {
+		if (_isComplete) {
+			Utils::Log("The bingo board is complete!");
+			return;
+		}
 
+		BingoNumber bingoNumber(number);
+
+		_bingoBoard[_currentRow].push_back(bingoNumber);
+
+		++_currentColumn %= _columnSize;
+
+		if (_currentColumn == 0) {
+			_currentRow++;
+		}
+
+		if (_currentRow >= _rowSize) {
+			_isComplete = true;
+		}
+	}
+
+	/// <summary>
+	/// Marks the parameter number and returns true if there is a winning row or column
+	/// </summary>
+	/// <param name="number"></param>
+	/// <returns></returns>
+	bool CheckNumber(int number) {
+
+		//Here we mark the number
+		int numberRow = 0;
+		int numberColumn = 0;
+
+		for (int i = 0; i < _rowSize; i++) {
+			for (int j = 0; j < _columnSize; j++) {
+				if (_bingoBoard[i][j]._number == number) {
+					_bingoBoard[i][j]._isMarked = true;
+					numberRow = i;
+					numberColumn = j;
+					break;
+				}
+			}
+		}
+
+		//Then check winning row
+		bool isWinningNumber = true;
+
+		for (int i = 0; i < _rowSize; i++) {
+			if (!_bingoBoard[i][numberColumn]._isMarked) {
+				isWinningNumber = false;
+				break;
+			}
+		}
+
+		if (!isWinningNumber) {
+			isWinningNumber = true;
+
+			//Finally check winning column, if no winning row
+			for (int i = 0; i < _columnSize; i++) {
+				if (!_bingoBoard[numberRow][i]._isMarked) {
+					isWinningNumber = false;
+					break;
+				}
+			}
+		}
+
+		if (isWinningNumber) {
+			_winNumber = number;
+			_isWinning = true;
+		}
+
+		return isWinningNumber;
+	}
+
+	int CalculateScore() {
+		int unmarkedSum = 0;
+
+		for (int i = 0; i < _rowSize; i++) {
+			for (int j = 0; j < _columnSize; j++) {
+				if (!_bingoBoard[i][j]._isMarked) {
+					unmarkedSum += _bingoBoard[i][j]._number;
+				}
+			}
+		}
+
+		return unmarkedSum * _winNumber;
+	}
+
+	bool IsComplete() {
+		return _isComplete;
+	}
+
+	bool IsWinning() {
+		return _isWinning;
+	}
+
+	void PrintBoard() {
+		for (int i = 0; i < _rowSize; i++) {
+			for (int j = 0; j < _columnSize; j++) {
+				std::cout << _bingoBoard[i][j]._number << " ";
+			}
+
+			std::cout << std::endl;
+		}
+
+		std::cout << "\n" << std::endl;
+	}
 };
 
 class Problem4 : public Problem
@@ -36,6 +152,9 @@ private:
 
 	//here we will load hte Random Numbers
 	void LoadProblemRandomNumbers(std::string filepath);
+
+	int FindFirstWinningBoard();
+	int FindLastWinningBoard();
 
 public:
 	Problem4(std::string filepath, std::string randomNumbersFilepath);
